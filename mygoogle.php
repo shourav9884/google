df4efc382b128e35ae9ff5be9b7ee5d39ecf8e58
Shourav <shourav9884@gmail.com>
2013-07-25 22:57:39 +0600

    first

---
+++ mygoogle.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="google.css">
+<head>
+
+</head>
+
+<body>
+<div class="headlist">
+<a href="https://www.google.com/" >+You</a>
+<a href="https://www.google.com/" >Search</a>
+<a href="https://www.google.com/" >Images</a>
+<a href="https://www.google.com/" >Maps</a>
+<a href="https://www.google.com/" >Play</a>
+<a href="https://www.google.com/" >Youtube</a>
+<a href="https://www.google.com/" >News</a>
+<a href="https://www.google.com/" >Gmail</a>
+<a href="https://www.google.com/" >Drive</a>
+</div>
+
+<button id="signin" type="button" >SIGN IN</button>
+
+<div id="mydiv">
+ <img src="logo4w.png" />
+
+<form>
+<input type="text" />
+</form>
+
+</div>
+
+</body>
+</html>
